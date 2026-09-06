7e242dfd4b94be4ff822c9a11b378621fee7f0f8
GiovanniVanDijk <152384349+GiovanniVanDijk@users.noreply.github.com>
2026-09-06 12:11:19 +0200

    Update quoter_templates.py

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -66,7 +66,7 @@ def page(content,user_id,title,error=None):
     return f"""<!DOCTYPE html>
 <html lang="en-US">
 <head>
-  <title>{title or "Quoter XP"}</title>
+  <title>{title or "Quoter XP V1"}</title> #hier versie aanpassen
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="/static/style.css">
 </head>
